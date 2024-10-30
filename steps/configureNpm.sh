d735e88c755e3c906538fd168ddbce76efdee834
Tim Kleyersburg <tk@wacg.de>
2024-10-30 11:04:40 +0100

    update nvm

--- steps/configureNpm.sh
+++ steps/configureNpm.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 
 # Install nvm (Node Version Manager)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 # Create a directory for global packages and tell npm where to store globally installed packages
 mkdir "${HOME}/.npm-packages"
+source "${HOME}/.zshrc"
 npm config set prefix "${HOME}/.npm-packages"
